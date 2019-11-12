b5b0a3ef1ca90134f39000f40d6be4eb6a18f8bd
jno <e.dvurechensky@ngslab.ru>
2019-11-12 14:04:52 +0300

    drop serial from term

--- sio_term.go
+++ sio_term.go
@@ -5,10 +5,9 @@ import "os"
 import "golang.org/x/crypto/ssh/terminal"
 
 const TTY = "/dev/tty"
-const Prompt = "> "
+const Prompt = ""
 
 type Console struct {
-	port *Port
 	tty *os.File
 	ttyFd int
 	con *terminal.Terminal
@@ -24,19 +23,14 @@ func (self *Console) Close() (e error) {
 	terminal.Restore(self.ttyFd, self.state)
 	self.ttyFd = -1
 	self.tty.Close()
-	if self.port != nil && self.port.IsOpen() {
-		self.port.Close()
-	}
 	return nil
 }
-func (self *Console) Open(p *Port) (e error) {
+func (self *Console) Open() (e error) {
 	defer func() {
 		if state := recover(); state != nil {
 			e = WrapError(state.(error))
 		}
 	}()
-
-	self.port = p
 
 	self.tty, e = os.OpenFile(TTY, os.O_RDWR, 0)
 	assert(e, "Open(%+q): %w", TTY, e)
@@ -52,38 +46,15 @@ func (self *Console) Open(p *Port) (e error) {
 	self.con = terminal.NewTerminal(self.tty, Prompt)
 	return nil
 }
-// RecvUntil() reads from serial port
-func (self *Console) RecvUntil(stops []string) (s string, e error) {
-	defer func() {
-		if state := recover(); state != nil {
-			e = WrapError(state.(error))
-		}
-	}()
-
-	s, e = self.port.ReadUntil(stops)
-	assert(e, "RecvUntil")
-	return s, nil
-}
-// Send() writes to the serial port
-func (self *Console) Send(s string) (e error) {
-	defer func() {
-		if state := recover(); state != nil {
-			e = WrapError(state.(error))
-		}
-	}()
-
-	self.port.WriteLine(s)
-	return nil
-}
 // Write() writes to user's screen
-func (self *Console) Write(s string) (e error) {
+func (self *Console) Write(s []byte) (e error) {
 	defer func() {
 		if state := recover(); state != nil {
 			e = WrapError(state.(error))
 		}
 	}()
 
-	self.con.Write([]byte(s))
+	self.con.Write(s)
 	return nil
 }
 // ReadLine() reads a line from user's keyboard
@@ -99,7 +70,7 @@ func (self *Console) ReadLine() (s string, e error) {
 	return s, nil
 }
 
-func NewConsole(p *Port) (con *Console, e error) {
+func NewConsole() (con *Console, e error) {
 	defer func() {
 		if state := recover(); state != nil {
 			e = WrapError(state.(error))
@@ -107,7 +78,7 @@ func NewConsole(p *Port) (con *Console, e error) {
 	}()
 
 	con = &Console{}
-	assert(con.Open(p), fmt.Sprintf("Console.Open(%+v)", p))
+	assert(con.Open(), fmt.Sprintf("Console.Open()"))
 
 	return con, nil
 }
