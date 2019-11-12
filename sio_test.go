67ecccae061b74c724d1f12a5a386b264c90fc42
jno <e.dvurechensky@ngslab.ru>
2019-11-12 15:54:31 +0300

    improve test code: single printf() + no extra select

--- sio_test.go
+++ sio_test.go
@@ -15,44 +15,45 @@ const USB0 = "/dev/ttyUSB0"
 var tty *Console
 var port *Port
 
+func printf(format string, args ...interface{}) (int, error) {
+	fmt.Fprintf(os.Stderr, format + "\r\n", args...)
+	return 0, nil
+}
+
 func sighand(sigchan chan os.Signal) {
 	for {
-		fmt.Println("Waiting for signal\r")
+		printf("Waiting for signal")
 		sig := <- sigchan // wait for a signal
-		if tty != nil { tty.Close(); fmt.Println("tty restored"); }
+		if tty != nil { tty.Close(); printf("tty restored"); }
 		panic(fmt.Sprintf("Killed with: %v", sig))
 	}
 }
 
 func userEnd(end chan bool, com chan []byte) {
-	fmt.Println("User start\r")
+	printf("User start")
 	for {
-		select {
-		case <- end: tty.Write([]byte("\r\nuser QUIT\r\n")); break
-		default:
-			if s, e := tty.ReadLine(); e != nil {
-				panic(e)
-			} else {
-				if strings.HasPrefix(s, "quit") {
-					end <- true
-					break
-				}
-				com <- []byte(s + "\r")
-			}
+		if s, e := tty.ReadLine(); e != nil {
+			panic(e)
+		} else if strings.HasPrefix(s, "quit") {
+			break
+		} else {
+			com <- []byte(s + "\r")
 		}
 	}
-	fmt.Println("User end\r")
+	end <- true
+	printf("User end")
 }
 
 func modemEnd(end chan bool, com chan []byte) {
-	fmt.Println("Modem start\r")
+	printf("Modem start")
 	for port.IsOpen() {
 		select {
 		case cmd := <- com: port.Write(cmd)
-		case <- end: tty.Write([]byte("\r\nmodem QUIT\r\n")); break
+		case <- end: printf("\r\nmodem QUIT"); break
 		default:
 			if n, e := port.InWaiting(); e != nil {
 				if e == PortNotOpenError {
+					printf("\r\nport lost")
 					break
 				}
 				panic(e)
@@ -67,28 +68,24 @@ func modemEnd(end chan bool, com chan []byte) {
 			}
 		}
 	}
-	fmt.Println("Modem end\r")
+	printf("Modem end")
 }
 
 func TestMain(t *testing.T) {
-	fmt.Println("begin")
+	printf("begin")
 
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT)
 	go sighand(sig)
 
 	port = NewSerialPort(USB0)
-	defer func() { port.Close(); fmt.Println("\r\nport closed\r"); }()
-	fmt.Println("Port is open:", port.String())
+	defer func() { port.Close(); printf("\r\nport closed"); }()
+	printf("Port is open: %s", port)
 
-	maj, min := port.DeviceId()
-	fmt.Printf("[%d:%d] %s\n", maj, min, port.DeviceClassName())
-
-	fmt.Println("Located:")
-	PrintLocations(port.SysFS(), fmt.Printf)
+	printf("Sys FS:"); PrintLocations(port.SysFS(), printf)
 
 	tty, _ = NewConsole()
-	defer func() { tty.Close(); fmt.Println("\r\ntty restored\r"); }()
+	defer func() { tty.Close(); printf("\r\ntty restored"); }()
 	tty.Write([]byte("\r\n"))
 
 	end := make(chan bool)		// termination mark
@@ -99,7 +96,7 @@ func TestMain(t *testing.T) {
 
 	<-end
 
-	fmt.Println("end")
+	printf("the end")
 }
 
 /* EOF */
